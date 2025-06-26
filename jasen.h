485935105f08ecff4ca482f8b07710d8375a4f90
Otto Mulari <omulari24@student.oulu.fi>
2025-06-26 23:22:01 +0300

    Update jasen.h

--- jasen.h
+++ jasen.h
@@ -3,8 +3,8 @@
 #include <vector>
 
 struct Kdata {
-    std::string type; // e.g., "button"
-    std::vector<std::string> styles; // e.g., "rounded", "hover", "blue"
+    std::string type; 
+    std::vector<std::string> styles; 
 };
 
 Kdata jasensyotto(const std::string& syotto);
